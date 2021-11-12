ec8e66e7c38f54c9a2ea8d051d6f9ca692ee4734
gaowu <2276872110@qq.com>
2021-11-12 21:21:18 +0800

    java

---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student{
+   private String name;
+   private String no;
+   private int age;
+   
+   Student(String name, String no, int age) {
+	this.name = name;
+	this.no = no;
+	this.age = age;
+    }
+}
+
